e040577c9fd9d9ac2032b0721a0b8767278c4ad6
Huan WANG <shrekwang1@163.com>
2016-08-19 14:42:18 +0800

    Initial Commit

---
+++ PureLayoutExtension/PureLayoutExtension.h
@@ -0,0 +1,13 @@
+//
+//  PureLayoutExtension.h
+//  PureLayoutExtension
+//
+//  Created by 王欢 on 8/19/16.
+//  Copyright © 2016 HuanWang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PureLayoutExtension : NSObject
+
+@end
---
+++ PureLayoutExtension/PureLayoutExtension.m
@@ -0,0 +1,13 @@
+//
+//  PureLayoutExtension.m
+//  PureLayoutExtension
+//
+//  Created by 王欢 on 8/19/16.
+//  Copyright © 2016 HuanWang. All rights reserved.
+//
+
+#import "PureLayoutExtension.h"
+
+@implementation PureLayoutExtension
+
+@end
